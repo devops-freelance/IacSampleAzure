f6c8ed4e5119525b00deb6ba535b6377bf5b6bf4
pintu-gupta <pintu.gupta@gmail.com>
2023-12-14 11:03:09 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,5 +5,4 @@ provider "azurerm" {
 resource "azurerm_resource_group" "resource_group" {
   name                     = var.resource_group_name 
   location                 = var.location
-  tags                     = var.tags
 }
